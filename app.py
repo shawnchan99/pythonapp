39410d2c9ffcf5959d5840d617b617cf419a85ce
shawnchan99 <47102035+shawnchan99@users.noreply.github.com>
2025-09-20 22:14:20 +0100

    Revert "Feature101"

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
 from flask import Flask
 app = Flask(__name__)
 
-# update version of Flask requires Werkzeug==2.2.2
-# specify the required version of Werkzeng to be 2.2.2 in requirements.txt
-
 @app.route("/")
 def hello():
    return "Hello World!"
